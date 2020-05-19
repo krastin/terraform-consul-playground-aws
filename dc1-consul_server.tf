36ae50493e078c5fe41b334f5d213bae95dc8ffc
Krastin Krastev <krastin@hashicorp.com>
2020-05-19 12:30:14 +0200

    first cleanup

---
+++ dc1-consul_server.tf
@@ -0,0 +1,69 @@
+resource "aws_instance" "krastin-consul-vm-node" {
+  count = length(var.dc1-consul_server_ips)
+  ami = data.aws_ami.consul.id
+  instance_type = "m5.large"
+  private_ip = var.dc1-consul_server_ips[count.index]
+
+  vpc_security_group_ids = ["${aws_security_group.krastin-consul-dc1-sg-permit.id}"]
+  subnet_id = aws_subnet.krastin-consul-dc1-subnet1.id
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # install consul binary
+      "sudo -H -u consul -s env VERSION='${var.dc1-consul_version}' /home/consul/install_consul.sh",
+
+      # set up the first #consul_servers amount of nodes as SERVER=true and as bootstrap-expect as the amount of server nodes
+      "sudo -H -u consul -s env RETRYIPS='${jsonencode(slice(var.dc1-consul_server_ips, 0, length(var.dc1-consul_server_ips)))}' SERVER='${count.index < length(var.dc1-consul_server_ips) ? "true" : "false"}' BOOTSTRAP='${length(var.dc1-consul_server_ips)}' /home/consul/configure_consul.sh",
+
+      # test auto-join
+      "sudo rm /etc/consul.d/retry_join.json",
+      #"echo -e 'retry_join = [\"provider=aws tag_key=CLUSTER tag_value=CONSUL\"]' | sudo tee /etc/consul.d/cloud_join.hcl",
+      "echo 'retry_join = [\"provider=aws tag_key=CLUSTER tag_value=CONSUL\"]' | sudo tee /etc/consul.d/cloud_join.hcl",
+
+      # start up consul
+      "sudo systemctl start consul"
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      agent = false
+      host = self.public_ip
+      private_key = file(var.ssh_key)
+    }
+  }
+
+  key_name = "krastin-key1"
+
+  tags = {
+    Owner = "krastin@hashicorp.com"
+    # Keep  = ""
+    Name = "krastin-consul-vm-node.${count.index}"
+    CLUSTER = "CONSUL"
+  }
+}
+
+data "aws_ami" "consul" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["krastin-xenial-consul-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["729476260648"]
+}
+
+output "consul_public_ips" {
+  value = zipmap(aws_instance.krastin-consul-vm-node.*.tags.Name, aws_instance.krastin-consul-vm-node.*.public_ip)
+  sensitive = false
+  description = "Public IP Addresses of nodes"
+}
--- main.tf
+++ main.tf
@@ -2,10 +2,43 @@ variable "aws_profile" {
   default = ""
 }
 variable "aws_region" {
-  default = "eu-west-1"
+  default = "eu-central-1"
+}
+
+variable "owner" {
+  default = "testuser@company.com"
+}
+
+variable "ssh_key" {
+  default = "~/.ssh/id_rsa"
 }
 
 provider "aws" {
-  profile    = "${var.aws_profile}"
-  region     = "${var.aws_region}"
+  profile    = var.aws_profile
+  region     = var.aws_region
+}
+
+# Consul DC1 related variables
+variable "dc1-cidr_block" {
+  default = "10.1.0.0/16"
+}
+
+variable "dc1-consul_version" {
+  default = "" # no version get newest OSS
+}
+
+variable "dc1-consul_server_ips" {
+  default = [
+    "10.1.0.101",
+    "10.1.0.102",
+    "10.1.0.103",
+  ]
+}
+
+variable "dc1-consul_client_ips" {
+  default = [
+    "10.1.0.201",
+    "10.1.0.202",
+    "10.1.0.203",
+  ]
 }
