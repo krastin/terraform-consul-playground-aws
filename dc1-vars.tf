419e248daa70fc17543a0157b82fd6cc2ad195d1
Krastin Krastev <krastin@hashicorp.com>
2020-05-19 12:34:57 +0200

    cleanup of main/dc1

---
+++ dc1-vars.tf
@@ -0,0 +1,25 @@
+
+# Consul DC1 related variables
+variable "dc1-cidr_block" {
+  default = "10.1.0.0/16"
+}
+
+variable "dc1-consul_version" {
+  default = "" # no version get newest OSS
+}
+
+variable "dc1-consul_server_ips" {
+  default = [
+    "10.1.0.101",
+    "10.1.0.102",
+    "10.1.0.103",
+  ]
+}
+
+variable "dc1-consul_client_ips" {
+  default = [
+    "10.1.0.201",
+    "10.1.0.202",
+    "10.1.0.203",
+  ]
+}
